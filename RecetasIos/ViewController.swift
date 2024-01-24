78097022813f9f9ff73cc92a4bdc0d9b2ea8b76b
marcos <marcoshmarc@gmail.com>
2024-01-24 12:23:30 +0100

    Initial Commit

---
+++ RecetasIos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecetasIos
+//
+//  Created by MarcosHernandez on 24/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
